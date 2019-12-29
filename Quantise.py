3b6aac752ea5d2f953100407f57d1fa4e09efb58
breusens <Brent.reusens@btinternet.com>
2019-12-29 19:14:34 +0000

    Revert "sync with playden"
    This reverts commit 30360a3faa133ef97c8004ea47dd2aefa2c3af0a.

--- Quantise.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def Quantise(X,step):
-    ((X+step//2)//step)*step
--- performance.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def performance(DF,filt,strow,enrow,stcol,encol,clrow0,clrow1,clcol0,clcol1):
-    DF[strow:enrow,stcol:encol]+=filt[clrow0:clrow1,clcol0:clcol1]
